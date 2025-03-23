70be8d03480920a2f15b7bf91879245734c96eab
MiguelBits <mcv.cooper@gmail.com>
2025-03-23 23:37:03 +0000

    figuring out how to use before liquidity

---
+++ src/3CRV69.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
+
+contract ThreeCRV69 is ERC20 {
+
+    mapping(uint256 tokenId => address token) public tokens;
+
+    modifier certifiedToken(uint256 tokenId) {
+        require(tokens[tokenId] != address(0), "Invalid tokenId");
+        _;
+    }
+
+    constructor(address _usdt, address _usdc, address _dai) ERC20("3CRV69", "3CRV69") {
+        tokens[0] = _usdt;
+        tokens[1] = _usdc;
+        tokens[2] = _dai;
+    }
+
+    function mint(uint256 _amount, uint256 _tokenId, address _to) public certifiedToken(_tokenId) {
+        ERC20 token = ERC20(tokens[_tokenId]);
+        //transfer tokenId to this contract
+        token.transferFrom(msg.sender, address(this), _amount);
+
+        //get token decimals
+        uint8 tokenDecimals = token.decimals();
+        uint256 amount18;
+        if (tokenDecimals != 18) {
+            //convert _amount to 18 decimals
+            amount18 = _amount * 10 ** (18 - tokenDecimals);
+        } else {
+            amount18 = _amount;
+        }
+
+        //mint 3crv69 to to
+        _mint(_to, amount18);
+    }
+
+    function burn(uint256 _amount, uint256 _tokenId, address _from) public certifiedToken(_tokenId) {
+        ERC20 token = ERC20(tokens[_tokenId]);
+        //burn 3crv69 from from
+        _burn(_from, _amount);
+
+        //get token decimals
+        uint8 tokenDecimals = token.decimals();
+        uint256 amountDecimals;
+        if (tokenDecimals != 18) {
+            //convert _amount to 18 decimals
+            amountDecimals = _amount / 10 ** (18 - tokenDecimals);
+        } else {
+            amountDecimals = _amount;
+        }
+
+        //transfer tokenId to from
+        token.transfer(msg.sender, amountDecimals);
+    }
+
+    function getToken(uint256 _tokenId) public view returns (address) {
+        return tokens[_tokenId];
+    }
+
+}
--- src/StabilityHook.sol
+++ src/StabilityHook.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.24; import "forge-std/console.sol";
 
 import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
 
@@ -9,8 +9,14 @@ import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
 
-contract Counter is BaseHook {
+import {ThreeCRV69} from "./3CRV69.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract StabilityHook is BaseHook {
     using PoolIdLibrary for PoolKey;
 
     // NOTE: ---------------------------------------------------------
@@ -22,9 +28,13 @@ contract Counter is BaseHook {
     mapping(PoolId => uint256 count) public afterSwapCount;
 
     mapping(PoolId => uint256 count) public beforeAddLiquidityCount;
-    mapping(PoolId => uint256 count) public beforeRemoveLiquidityCount;
+    mapping(PoolId => uint256 count) public afterRemoveLiquidityCount;
 
-    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+    ThreeCRV69 public threeCRV69_contract;
+
+    constructor(IPoolManager _poolManager, ThreeCRV69 _threeCRV69) BaseHook(_poolManager) {
+        threeCRV69_contract = _threeCRV69;
+    }
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
@@ -32,8 +42,8 @@ contract Counter is BaseHook {
             afterInitialize: false,
             beforeAddLiquidity: true,
             afterAddLiquidity: false,
-            beforeRemoveLiquidity: true,
-            afterRemoveLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: true,
             beforeSwap: true,
             afterSwap: true,
             beforeDonate: false,
@@ -68,22 +78,44 @@ contract Counter is BaseHook {
     }
 
     function _beforeAddLiquidity(
-        address,
+        address sender,
         PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
+        IPoolManager.ModifyLiquidityParams calldata params,
+        bytes calldata hookData
     ) internal override returns (bytes4) {
         beforeAddLiquidityCount[key.toId()]++;
+
+        console.log("beforeAddLiquidity");
+        console.log("sender balance0", IERC20(Currency.unwrap(key.currency0)).balanceOf(sender));    
+        console.log("sender balance1", IERC20(Currency.unwrap(key.currency1)).balanceOf(sender));
+        
+        //decode the params into address
+        (uint256 token0_id) = abi.decode(hookData, (uint256));
+        address token0 = threeCRV69_contract.getToken(token0_id);
+        
+        uint256 token0_amount = uint256(params.liquidityDelta);
+
+        //check if the token0 is the threeCRV69 contract
+        if (token0 != address(0)) {
+            //transfer token0 to the pool
+            IERC20(token0).transferFrom(sender, address(this), token0_amount);
+            console.log("balance of token0", IERC20(token0).balanceOf(address(this)));
+            //mint 3crv69 to the user
+            threeCRV69_contract.mint(token0_amount, token0_id, sender);
+        }
+
         return BaseHook.beforeAddLiquidity.selector;
     }
 
-    function _beforeRemoveLiquidity(
+    function _afterRemoveLiquidity(
         address,
         PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
-    ) internal override returns (bytes4) {
-        beforeRemoveLiquidityCount[key.toId()]++;
-        return BaseHook.beforeRemoveLiquidity.selector;
+        IPoolManager.ModifyLiquidityParams calldata /*params*/,
+        BalanceDelta delta,
+        BalanceDelta /*feesAccrued*/,
+        bytes calldata /*hookData*/
+    ) internal override returns (bytes4, BalanceDelta) {
+        afterRemoveLiquidityCount[key.toId()]++;
+        return (BaseHook.afterRemoveLiquidity.selector, delta);
     }
 }
